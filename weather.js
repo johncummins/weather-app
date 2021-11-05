234ff419d90e034db24d8ca9659a3991624d9c39
johncummins <johncummins1997@gmail.com>
2021-11-05 18:09:33 +0000

    Added time until sunrise/sunset + added responsive css rules + general tidy up

--- weather.js
+++ weather.js
@@ -1,6 +1,5 @@
 // "https://api.openweathermap.org/data/2.5/weather?q=London&appid=1ea2c468f0cee30b1e01b9e6a2258346"
 const currentTime = Math.round(new Date().getTime()); // THis is in milliseconds
-
 $(document).ready(function () {
     $('.main__search-box--form').on('submit', () => {
         // prevents default behaviour
@@ -16,7 +15,7 @@ $("#search-button").click(function () {
 
 // takes care of enter button
 $('.main__search-box--form').keypress((e) => {
-    // Enter key = number 13
+
     if (e.which === 13) {
         $('.main__search-box--form').submit();
         console.log('form submitted through enter key');
@@ -25,6 +24,8 @@ $('.main__search-box--form').keypress((e) => {
 
 function getWeather() {
     var city_name = $("#input-box").val();
+    console.log("THis is the city name: ", city_name)
+
     var url = "https://api.openweathermap.org/data/2.5/weather?q=" + city_name + "&appid=" + config.api_key;
     console.log("This is the text:", city_name)
     $.get(url, function (data, status) {
@@ -41,7 +42,6 @@ function getWeather() {
         const icon = data.weather[0].icon;
         // $('#main__weather-output').text(desc);
         showWeather(icon, kelvin);
-        getTimeTill(sunrise, sunset);
         getNightDay(sunrise, sunset, timezone);
     });
 }
@@ -56,47 +56,18 @@ function showWeather(icon, kelvin) {
     celsius = +celsius.toFixed(2);
     console.log(celsius)
     $('#main__weather-results-text').text(celsius);
+    $('#main__weather-results-text').append("&deg;C");
+    // $('#main__weather-results-text').css("content:", "'\00b0 C' ");
     // $(".main__weather-results-box").css({ top: 'auto' });
     $(".main__weather-results-box").animate({
         height: '200px',
-        bottom: "0px",
     }, 300);
-}
-
-function getTimeTill(sunrise, sunset) {
-
-    console.log("sunrise   : ", sunrise);
-    console.log("Current: ", currentTime);
-    let timeString = "";
-
-    if (currentTime > sunrise) {
-        // sunrise was in the past
-        const timeDiffMilPast = currentTime - sunrise;
-        timeDiffHourPast = convertDiffMillToHours(timeDiffMilPast);
-        timeString = "Sunrise was " + timeDiffHourPast + " hours ago"
-        console.log(timeString);
-    }
-    else {
-        // sunrise is in the Future
-        const timeDiffMilFut = sunset - currentTime;
-        timeDiffHourFut = convertDiffMillToHours(timeDiffMilFut);
-        timeString = "Sunset in " + timeDiffHourPast + " hours"
-        console.log(timeString);
-
-    }
-    $("#main__time-results").text(timeString)
-
-}
-
-function convertDiffMillToHours(timeDiffMil) {
-    var differenceDate = new Date(timeDiffMil);
-    var diffHours = differenceDate.getUTCHours();
-    // console.log("Time DIff: " + diffHours + "(in hours)");
-    return diffHours;
 }
 
 function getNightDay(sunrise, sunset, timezone) {
     let adjustedTime;
+    var isNightime;
+
     if (timezone !== NaN) {
         adjustedTime = currentTime;
     }
@@ -104,29 +75,78 @@ function getNightDay(sunrise, sunset, timezone) {
         adjustedTime = currentTime - timezone;
     }
 
-    console.log(adjustedTime);
-    console.log("TimeZ: ", timezone);
-    console.log("Curr", currentTime);
     if (sunset < adjustedTime && adjustedTime > sunrise) {
-        console.log("Its night time!");
+        isNightime = true;
 
-        // $(".main__search-box--img").css({
-        //     animation: 'decrease-brightness',
-        // })
-        // filter: brightness(0.6);
+        console.log("Its night time!, nightime var = ", isNightime);
 
         $(".main__search-box--img").removeClass("main__search-box--img__day");
         $(".main__search-box--img").addClass("main__search-box--img__night");
-
-
     }
     else {
-        console.log("Its day time!");
+        isNightime = false;
+        console.log("Its day time!, nightime var = ", isNightime);
+
         $(".main__search-box--img").removeClass("main__search-box--img__night");
         $(".main__search-box--img").addClass("main__search-box--img__day");
+    }
+    //Call the func to get sunrise and sunset
+    getTimeTill(sunrise, sunset, isNightime);
 
+}
+
+function getTimeTill(sunrise, sunset, isNightime) {
+
+    let timeString = "";
+    // sunrise is in the Future
+    const tDiffMilRiseFut = sunrise - currentTime;
+    // sunrise was in the past
+    const tDiffMilRisePast = currentTime - sunrise;
+    // sunset is in the future
+    const tDiffMilSetFut = sunset - currentTime;
+    // sunset was in the past
+    const tDiffMilSetPast = currentTime - sunset;
+
+    if (isNightime === true) {
+        // show sunrise
+        console.log("sunrise is in... ")
+        if (tDiffMilRiseFut < 0) {
+            // display the past
+            tDiffHourRisePast = convertDiffMillToHours(tDiffMilRisePast);
+            timeString = "Sunrise was " + tDiffHourRisePast + " hours ago"
+
+        }
+
+        else {
+            // display the future
+            tDiffHourRiseFut = convertDiffMillToHours(tDiffMilRiseFut);
+            timeString = "Sunrise in " + tDiffHourRiseFut + " hours"
+        }
+    }
+    else {
+        // show sunset
+        console.log("sunset is in... ");
+        if (tDiffMilSetFut < 0) {
+            // display the past
+            tDiffHourSetPast = convertDiffMillToHours(tDiffMilSetPast);
+            timeString = "Sunset was " + tDiffHourSetPast + " hours ago"
+        }
+
+        else {
+            // display the future
+            tDiffHourSetFut = convertDiffMillToHours(tDiffMilSetFut);
+            timeString = "Sunset in " + tDiffHourSetFut + " hours"
+        }
 
     }
 
+    $("#main__time-results").text(timeString)
+}
+
+
+function convertDiffMillToHours(timeDiffMil) {
+    var differenceDate = new Date(timeDiffMil);
+    var diffHours = differenceDate.getUTCHours();
+    return diffHours;
 }
 
